d8e549e032e0febc27f45da32320ace711427a15
Zacheriah Wayman <waymanzacheriah@gmail.com>
2020-12-22 11:25:31 -0500

    Top posts and controversial posts are working

--- src/main/java/com/bot/features/Listener.java
+++ src/main/java/com/bot/features/Listener.java
@@ -8,26 +8,59 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static com.bot.features.TopPostsOfTheWeek.returnMostUpvoted;
+import static com.bot.features.TopPostsOfTheWeek.returnTopMessage;
 import static net.dv8tion.jda.api.entities.MessageHistory.getHistoryAfter;
+import static net.dv8tion.jda.api.entities.MessageHistory.getHistoryBefore;
 
 public class Listener extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event){
         Message message = event.getMessage();
+        MessageChannel channel = message.getChannel();
+
         switch(message.getContentDisplay()){
-            case "!help":
-                MessageChannel channel = message.getChannel();
-                System.out.println("debug");
+            case "!help": {
                 channel.sendMessage("For feature requests, contact <@145717043289784320>! Current features include: \n" +
                         "- Birthday Announcer\n" +
-                        "- Top posts of the week(WIP)").queue();
-                System.out.println("debug");
+                        "- Top posts of the week(!top)\n" +
+                        "- Most controversial post of the week(!controversial)").queue();
                 break;
+            }
 
-            case "!top":
-                MessageHistory messageHistory = getHistoryAfter(message.getChannel(), );
+            case "!top": {
+
+                CountDownLatch latch = new CountDownLatch(1);
+                List<Message> messageList = new ArrayList<Message>();
+                try {
+                    channel.getHistory().retrievePast(100).queue(messages -> messageList.addAll(messages));
+                    latch.await(1, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Message topMessage = returnMostUpvoted(messageList);
+                channel.sendMessage("The top message of the past 100 messages is! " + topMessage.getJumpUrl()).queue();
                 break;
+            }
+
+            case "!controversial": {
+                CountDownLatch latch = new CountDownLatch(1);
+                List<Message> messageList = new ArrayList<Message>();
+                try {
+                    channel.getHistory().retrievePast(100).queue(messages -> messageList.addAll(messages));
+                    latch.await(1, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Message topMessage = returnTopMessage(messageList);
+                channel.sendMessage("The most reacted message of the past 100 messages is! " + topMessage.getJumpUrl()).queue();
+                break;
+            }
         }
         if (event.isFromType(ChannelType.PRIVATE))
         {
--- src/main/java/com/bot/features/TopPostsOfTheWeek.java
+++ src/main/java/com/bot/features/TopPostsOfTheWeek.java
@@ -1,8 +1,55 @@
 package com.bot.features;
 
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageReaction;
+
+import java.util.List;
 public class TopPostsOfTheWeek {
 
-    public TopPostsOfTheWeek(){
+    static public Message returnTopMessage(List<Message> messageList){
+        Message topMessage = messageList.get(0);
 
+        for(int i = 0; i < messageList.size(); i++){
+            int reactions1 = numberOfReactions(messageList.get(i).getReactions());
+            int reactions2 = numberOfReactions(topMessage.getReactions());
+            if(reactions1 > reactions2){
+                topMessage = messageList.get(i);
+            }
+        }
+        return topMessage;
+    }
+
+    static public int numberOfReactions(List<MessageReaction> reactions){
+        int totalReactions = 0;
+        for(MessageReaction reaction : reactions){
+            totalReactions += reaction.getCount();
+        }
+        return totalReactions;
+    }
+
+    static public int numberOfUpvotes(List<MessageReaction> reactions){
+        int totalReactions = 0;
+        for(MessageReaction reaction : reactions){
+            if(reaction.getReactionEmote().getEmote().getName().equals("UpVote")){
+                totalReactions += reaction.getCount();
+            }else if(reaction.getReactionEmote().getEmote().getName().equals("DownVote")){
+                totalReactions = totalReactions - reaction.getCount();
+            }
+        }
+        return totalReactions;
+    }
+
+    static public Message returnMostUpvoted(List<Message> messageList){
+        Message mostUpvoted = messageList.get(0);
+
+        for(int i = 0; i < messageList.size(); i++){
+            int reactions1 = numberOfUpvotes(messageList.get(i).getReactions());
+            int reactions2 = numberOfUpvotes(mostUpvoted.getReactions());
+
+            if(reactions1 > reactions2){
+                mostUpvoted = messageList.get(i);
+            }
+        }
+        return mostUpvoted;
     }
 }
