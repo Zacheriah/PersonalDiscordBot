feadf8d0e7b46ee7f048b1c1a0bd47b479695881
Zacheriah Wayman <waymanzacheriah@gmail.com>
2020-12-22 23:25:20 -0500

    Added !monthly command to get posts of the month

--- src/main/java/com/bot/features/Listener.java
+++ src/main/java/com/bot/features/Listener.java
@@ -7,11 +7,13 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.OffsetDateTime;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static com.bot.features.TopPostsOfTheWeek.*;
+import static net.dv8tion.jda.api.utils.TimeUtil.getDiscordTimestamp;
 
 public class Listener extends ListenerAdapter {
 
@@ -20,56 +22,104 @@ public class Listener extends ListenerAdapter {
         Message message = event.getMessage();
         MessageChannel channel = message.getChannel();
 
+        long DAY_IN_MS = 1000 * 60 * 60 * 24;
+        Date date = new Date(System.currentTimeMillis() - (7*DAY_IN_MS));
+        OffsetDateTime offsetDateTime = date.toInstant().atOffset(message.getTimeCreated().getOffset());
+
         switch(message.getContentDisplay()){
             case "!help": {
                 channel.sendMessage("For feature requests, contact <@145717043289784320>! Current features include: \n" +
                         "- Birthday Announcer\n" +
                         "- Top posts of the week (!top)\n" +
                         "- Most controversial post of the week (!controversial)\n" +
-                        "- Most downvoted post of the week (!worst)").queue();
+                        "- Most downvoted post of the week (!worst)\n" +
+                        "- Posts of the month (!monthly)").queue();
                 break;
             }
 
             case "!top": {
-
-                CountDownLatch latch = new CountDownLatch(1);
                 List<Message> messageList = new ArrayList<Message>();
-                try {
-                    channel.getHistory().retrievePast(100).queue(messages -> messageList.addAll(messages));
-                    latch.await(1, TimeUnit.SECONDS);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+                for(Message currentMessage : channel.getIterableHistory()){
+                    if(currentMessage.getTimeCreated().isAfter(offsetDateTime)){
+                        messageList.add(currentMessage);
+                    }else if (messageList.size() > 500){
+                        break;
+                    }else{
+                        break;
+                    }
                 }
+
                 Message topMessage = returnMostUpvoted(messageList);
-                channel.sendMessage("The top message of the past 100 messages is: " + topMessage.getJumpUrl()).queue();
+
+                System.out.print("for loop done wowzers");
+                channel.sendMessage("The top message of the past week is: " + topMessage.getJumpUrl()).queue();
                 break;
             }
 
             case "!controversial": {
-                CountDownLatch latch = new CountDownLatch(1);
                 List<Message> messageList = new ArrayList<Message>();
-                try {
-                    channel.getHistory().retrievePast(100).queue(messages -> messageList.addAll(messages));
-                    latch.await(1, TimeUnit.SECONDS);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+                for(Message currentMessage : channel.getIterableHistory()){
+                    if(currentMessage.getTimeCreated().isAfter(offsetDateTime)){
+                        messageList.add(currentMessage);
+                    }else if (messageList.size() > 500){
+                        break;
+                    }else{
+                        break;
+                    }
                 }
+
                 Message topMessage = returnMostReacted(messageList);
-                channel.sendMessage("The most reacted message of the past 100 messages is: " + topMessage.getJumpUrl()).queue();
+
+                System.out.print("for loop done wowzers");
+                channel.sendMessage("The most controversial message of the past week is: " + topMessage.getJumpUrl()).queue();
                 break;
             }
 
             case "!worst": {
-                CountDownLatch latch = new CountDownLatch(1);
                 List<Message> messageList = new ArrayList<Message>();
-                try {
-                    channel.getHistory().retrievePast(100).queue(messages -> messageList.addAll(messages));
-                    latch.await(1, TimeUnit.SECONDS);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+                for(Message currentMessage : channel.getIterableHistory()){
+                    if(currentMessage.getTimeCreated().isAfter(offsetDateTime)){
+                        messageList.add(currentMessage);
+                    }else if (messageList.size() > 500){
+                        break;
+                    }else{
+                        break;
+                    }
                 }
+
                 Message topMessage = returnMostDownvoted(messageList);
-                channel.sendMessage("The most downvoted message of the past 100 messages is: " + topMessage.getJumpUrl()).queue();
+
+                System.out.print("for loop done wowzers");
+                channel.sendMessage("The WORST message of the past week is: " + topMessage.getJumpUrl()).queue();
+                break;
+            }
+
+            case "!monthly": {
+                Date date2 = new Date(System.currentTimeMillis() - (30*DAY_IN_MS));
+                OffsetDateTime offsetDateTime2 = date2.toInstant().atOffset(message.getTimeCreated().getOffset());
+                List<Message> messageList = new ArrayList<Message>();
+
+                for(Message currentMessage : channel.getIterableHistory()){
+                    if(currentMessage.getTimeCreated().isAfter(offsetDateTime2)){
+                        messageList.add(currentMessage);
+                    }else if (messageList.size() > 500){
+                        break;
+                    }else{
+                        break;
+                    }
+                }
+
+                Message topMessage = returnMostUpvoted(messageList);
+                Message controversialMessage = returnMostReacted(messageList);
+                Message bottomMessage = returnMostDownvoted(messageList);
+                System.out.print("for loop done wowzers");
+                channel.sendMessage("Here's the roundup for the month: \n" +
+                        "The top message of the past month is: " + topMessage.getJumpUrl() + "\n" +
+                        "The most controversial message of the past month is: " + controversialMessage.getJumpUrl() + "\n" +
+                        "The worst message of the past month is: " + bottomMessage.getJumpUrl()).queue();
                 break;
             }
         }
